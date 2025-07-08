90e738fce9c49d4b43ebab65faef02476a56deaf
Tom Swokowski <swokowskithomas@gmail.com>
2025-07-08 17:16:13 -0500

    update site nav

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    this.lastScroll = window.scrollY
+    this.handleScroll = this.handleScroll.bind(this)
+    window.addEventListener("scroll", this.handleScroll)
+    this.threshold = 100
+  }
+
+  disconnect() {
+    window.removeEventListener("scroll", this.handleScroll)
+  }
+
+  handleScroll() {
+    const currentScroll = window.scrollY
+
+    if (currentScroll <= this.threshold) {
+      this.element.classList.remove("-translate-y-full")
+      return
+    }
+
+    if (currentScroll > this.lastScroll) {
+      this.element.classList.add("-translate-y-full")
+    } else {
+      this.element.classList.remove("-translate-y-full")
+    }
+
+    this.lastScroll = currentScroll
+  }
+}
