cdab279dd3b6e65e5064cd7fee7b1f8c938812ee
Tom Swokowski <swokowskithomas@gmail.com>
2025-07-11 18:34:29 -0500

    add admin email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   before_validation :normalize_email
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
+  ADMIN_EMAIL = ENV.fetch("ADMIN_EMAIL")
+
   def self.from_omniauth(auth)
     email = auth.info.email.to_s.strip.downcase
 
@@ -15,6 +17,10 @@ class User < ApplicationRecord
     raise "Unverified email from OAuth provider" unless verified
 
     find_or_create_by!(email: email)
+  end
+
+  def admin?
+    email == ADMIN_EMAIL
   end
 
   private
