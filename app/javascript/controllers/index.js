29adf2cacdca6038dae9cd77fe19c9206bd6beb5
Thomas Swokowski <swokowskithomas@gmail.com>
2025-07-21 11:56:19 -0700

    prettier setup updates

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,10 @@
-import { application } from 'controllers/application'
-import { eagerLoadControllersFrom } from '@hotwired/stimulus-loading'
+import { application } from 'controllers/application';
+import { eagerLoadControllersFrom } from '@hotwired/stimulus-loading';
 
 // auto‑load local controllers
-eagerLoadControllersFrom('controllers', application)
+eagerLoadControllersFrom('controllers', application);
 
 // Import & Register the Marksmith editor controllers
-import { MarksmithController, ListContinuationController } from '@avo-hq/marksmith'
-application.register('marksmith',         MarksmithController)
-application.register('list-continuation', ListContinuationController)
+import { MarksmithController, ListContinuationController } from '@avo-hq/marksmith';
+application.register('marksmith', MarksmithController);
+application.register('list-continuation', ListContinuationController);
