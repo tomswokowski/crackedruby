3bcd5c5c1d04cdd100eb11fb1c0d626fa5ca3791
Thomas Swokowski <swokowskithomas@gmail.com>
2025-07-29 15:57:10 -0500

    revert nav changes

--- app/javascript/controllers/navigation_drawer_controller.js
+++ app/javascript/controllers/navigation_drawer_controller.js
@@ -59,14 +59,14 @@ export default class extends Controller {
   open() {
     this.panelTarget.classList.remove('-translate-x-full');
     this.backdropTarget.classList.remove('hidden');
-    this.closeButtonTarget.style.left = '260px';
+    this.closeButtonTarget.classList.remove('hidden');
     document.body.classList.add('overflow-hidden');
   }
 
   close() {
     this.panelTarget.classList.add('-translate-x-full');
     this.backdropTarget.classList.add('hidden');
-    this.closeButtonTarget.style.left = '-28px';
+    this.closeButtonTarget.classList.add('hidden');
     document.body.classList.remove('overflow-hidden');
   }
 
