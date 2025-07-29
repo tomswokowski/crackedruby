da1dfe1ccd1e11a2e2d75169bca006762b4034d5
Thomas Swokowski <swokowskithomas@gmail.com>
2025-07-29 16:57:52 -0500

    add scroll top on turbo navigation

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,4 +5,8 @@ const application = Application.start();
 application.debug = false;
 window.Stimulus = application;
 
+document.addEventListener('turbo:load', () => {
+  window.scrollTo(0, 0);
+});
+
 export { application };
