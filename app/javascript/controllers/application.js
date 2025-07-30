006d11dc08aaddc76de12e82bfb502a89e1b689d
Thomas Swokowski <swokowskithomas@gmail.com>
2025-07-29 23:08:30 -0500

    clean up js code

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,7 +6,14 @@ application.debug = false;
 window.Stimulus = application;
 
 document.addEventListener('turbo:load', () => {
-  window.scrollTo(0, 0);
+  const isIOSChrome = /CriOS/.test(navigator.userAgent);
+
+  if (isIOSChrome) {
+    window.scrollTo(0, -1);
+    setTimeout(() => window.scrollTo(0, 0), 1);
+  } else {
+    window.scrollTo(0, 0);
+  }
 });
 
 export { application };
