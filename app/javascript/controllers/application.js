83095b2abca3c88bc4d5c2ea9367d05b6ccdce5e
Thomas Swokowski <swokowskithomas@gmail.com>
2025-07-29 22:37:44 -0500

    fix scroll to top issue

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,11 +6,7 @@ application.debug = false;
 window.Stimulus = application;
 
 document.addEventListener('turbo:load', () => {
-  setTimeout(() => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    window.scrollTo(0, 0);
-  }, 0);
+  window.scrollTo(0, 0);
 });
 
 export { application };
